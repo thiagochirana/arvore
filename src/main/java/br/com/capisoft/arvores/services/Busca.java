bcde13bef11cb233d7cf8e8cee062d144126fcd7
Thiago Chirana <engsw.thiago@gmail.com>
2023-09-11 22:20:58 -0300

    IMPROVEMENT | Ajuste para deixar código mais clean, remoção de metodos não usados

--- src/main/java/br/com/capisoft/arvores/services/Busca.java
+++ src/main/java/br/com/capisoft/arvores/services/Busca.java
@@ -23,56 +23,6 @@ public class Busca {
         return no;
     }
 
-    public Node binariaNoNode(Node node, String textoDoNodeBuscado){
-        LOG.info("VOU REALIZAR UMA BUSCA BINÁRIA NA ARVORE "+node.toString());
-        Node no = buscaBinaria(node, textoDoNodeBuscado);
-        if (no == null) {
-            LOG.info("BUSCA BINARIA | NADA ENCONTRADO | Node \""+textoDoNodeBuscado.toUpperCase()+"\" NÃO encontrado a partir do Node \""+node.getPalavra().toUpperCase()+"\"\n----------------------------------------------\n");
-        } else {
-            LOG.info("BUSCA BINARIA | NADA ENCONTRADO | Node \""+textoDoNodeBuscado.toUpperCase()+"\" foi encontrado a partir do Node \""+node.getPalavra().toUpperCase()+"\"\n----------------------------------------------\n");
-        }
-        return no;
-    }
-
-    public int obterAlturaDoNode(Arvore arvore, Node node){
-        int a = verificarAlturaBuscaBinaria(arvore.getRoot(),node);
-        LOG.info("VERIFICACAO DE ALTURA | A altura atual do "+node+" é "+a);
-        return a;
-    }
-
-    public int obterNiveis(Node node){
-        int v = niveis(node);
-        return v;
-    }
-
-    public Node buscarNodeDesbalanceado(Node root){
-        if (root == null){
-            return null;
-        }
-        int fator = obterNiveis(root.getNoDireito()) - obterNiveis(root.getNoEsquerdo());
-        if (fator < -2 || fator > 2) {
-            return root;
-        } else {
-            buscarNodeDesbalanceado(root.getNoEsquerdo());
-            buscarNodeDesbalanceado(root.getNoDireito());
-        }
-        return null;
-    }
-
-    private int niveis(Node node){
-        int esq = 0;
-        int dir = 0;
-
-        if (node == null) {
-            return 0;
-        }
-
-        esq = niveis(node.getNoEsquerdo());
-        dir = niveis(node.getNoDireito());
-
-        return Math.max(esq, dir) + 1;
-    }
-
     private Node buscaBinaria(Node node, String texto){
         LOG.info("BUSCA BINARIA | Buscando \""+texto.toUpperCase()+"\", a partir do Node \""+node.toString()+"\"");
 
@@ -99,28 +49,4 @@ public class Busca {
         return null;
     }
 
-    private int verificarAlturaBuscaBinaria(Node raiz, Node nodeBuscado){
-        int cont = 0;
-        if (nodeBuscado == null){
-            return cont;
-        }
-        String buscado = nodeBuscado.getPalavra();
-        if (raiz.getPalavra().equals(buscado)){
-            return cont;
-        }
-        int res = raiz.getPalavra().compareTo(buscado);
-        if (res == 0){
-            return cont;
-        }
-        if (res > 0){
-            if (raiz.contemNoEsquerdo()){
-                cont += verificarAlturaBuscaBinaria(raiz.getNoEsquerdo(), nodeBuscado) + 1;
-            }
-        } else {
-            if (raiz.contemNoDireito()){
-                cont += verificarAlturaBuscaBinaria(raiz.getNoDireito(), nodeBuscado) + 1;
-            }
-        }
-        return cont;
-    }
 }
